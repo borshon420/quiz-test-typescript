6b0d6d23c85b896ddc630871049ab11b24d94012
borshon420 <borshonarafat420@gmail.com>
2021-12-28 11:38:17 +0600

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+
+const Home = lazy(()=> import('./pages/Home'));
+const Exam = lazy(()=> import('./pages/Exam'));
+const Result = lazy(()=> import('./pages/Result'));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense fallback={<p>Loading...</p>}>
+      <BrowserRouter>
+          <Routes>
+            <Route path={"/"} element={<Home/>}></Route>
+            <Route path={"/exam"} element={<Exam/>}></Route>
+            <Route path={"/result"} element={<Result/>}></Route>
+          </Routes>
+      </BrowserRouter>
+      </Suspense>
     </div>
   );
 }
---
+++ src/pages/Result.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Result = () => {
+    return (
+        <div>
+            <h2>This is result</h2>
+        </div>
+    );
+};
+
+export default Result;
